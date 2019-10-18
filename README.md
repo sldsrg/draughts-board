1ddc5065f214a4d9731a5a8a0c3c9b8f253f96b2
Sergey Solodukhin <soloss@yandex.ru>
2019-10-18 12:44:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Interactive Draughts Board
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export class Board extends Component {
+  public render() {
+    return <div>Draughts Board</div>
+  }
+}
