69579f6ae643d781dd8c134048d5f0aa56bb5ef5
Sergey Solodukhin <soloss@yandex.ru>
2019-10-28 20:57:38 +0300

    draw piece with path

--- src/piece.tsx
+++ src/piece.tsx
@@ -12,12 +12,14 @@ export class Piece {
 
 const useStyles = createUseStyles({
   whitePiece: {
-    stroke: '#000000',
+    fill: 'white',
+    stroke: 'darkGray',
     strokeWidth: 2,
     transition: '1s'
   },
   blackPiece: {
-    stroke: '#ffffff',
+    fill: 'black',
+    stroke: 'lightGray',
     strokeWidth: 2,
     transition: '1s'
   }
@@ -29,21 +31,30 @@ interface IProps {
 
 export function Glyph(props: IProps) {
   const classes = useStyles()
-  const cx = props.piece.column * 80 + 40
-  const cy = props.piece.row * 80 + 40
+  const x = props.piece.column * 80 + 40
+  const y = props.piece.row * 80 + 40
+  const pathData = `
+    M ${x},${y}
+    m -36,0 a 36,36 0 1,0 72,0 a 36,36 0 1,0 -72,0
+    m 8,0 a 28,28 0 1,0 56,0 a 28,28 0 1,0 -56,0
+    m 8,0 a 20,20 0 1,0 40,0 a 20,20 0 1,0 -40,0
+  `
   return props.piece.isWhite ? (
-    <>
-      <circle className={classes.whitePiece} fill='white' cx={cx} cy={cy} r={36} />
-      <circle className={classes.whitePiece} fill='transparent' cx={cx} cy={cy} r={29} />
-      <circle className={classes.whitePiece} fill='transparent' cx={cx} cy={cy} r={20} />
-      <circle className={classes.whitePiece} fill='transparent' cx={cx} cy={cy} r={16} />
-    </>
+    <path d={pathData} className={classes.whitePiece}></path>
   ) : (
-    <>
-      <circle className={classes.blackPiece} fill='black' cx={cx} cy={cy} r={36} />
-      <circle className={classes.blackPiece} fill='transparent' cx={cx} cy={cy} r={29} />
-      <circle className={classes.blackPiece} fill='transparent' cx={cx} cy={cy} r={20} />
-      <circle className={classes.blackPiece} fill='transparent' cx={cx} cy={cy} r={16} />
-    </>
+    // <>
+    //   <circle className={classes.whitePiece} fill='white'  r={36} />
+    //   <circle className={classes.whitePiece} fill='transparent' cx={cx} cy={cy} r={29} />
+    //   <circle className={classes.whitePiece} fill='transparent' cx={cx} cy={cy} r={20} />
+    //   <circle className={classes.whitePiece} fill='transparent' cx={cx} cy={cy} r={16} />
+    // </>
+    <path d={pathData} className={classes.blackPiece}></path>
+
+    // <>
+    //   <circle className={classes.blackPiece} fill='black' cx={cx} cy={cy} r={36} />
+    //   <circle className={classes.blackPiece} fill='transparent' cx={cx} cy={cy} r={29} />
+    //   <circle className={classes.blackPiece} fill='transparent' cx={cx} cy={cy} r={20} />
+    //   <circle className={classes.blackPiece} fill='transparent' cx={cx} cy={cy} r={16} />
+    // </>
   )
 }
